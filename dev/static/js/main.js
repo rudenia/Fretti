de7ea03e08f8771c8647e1f4da8edd8bd5b4ed23
rudenia <45589318+rudenia@users.noreply.github.com>
2023-10-03 20:56:13 +0200

    Initial commit

---
+++ dev/static/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function (){
+    $('.header__regular').slick({
+        adaptiveHeight: false,
+        speed: 1000,
+    });
+    $('.about__main__review').slick({
+        adaptiveHeight: false,
+        speed: 1000,
+        arrows: false,
+        dots: true,
+    });
+
+})
